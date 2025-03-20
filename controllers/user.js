56a215d7916e8419bf68256afa5b09d6a28c210d
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-03-21 01:38:04 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -7,19 +7,14 @@ exports.postHome=(req,res,next)=>{
     const dobs=new Date(dob);
     const avgHumnlife=70;
     const date=new Date();
-    // const dates=date.toISOString().split('T')[0];
     let dif=date.getTime()-dobs.getTime();
-    // console.log(dates);
     dif=dif/(1000*60*60*24*365.25);
     dif=Math.floor(dif);
     let total_life_year=avgHumnlife-dif;
     if(total_life_year>0){
-        console.log(total_life_year);
         let todays=total_life_year*365.25;
-        
         let hour=todays-parseInt(todays);
         hour=hour*24;
-        console.log(todays,"days");
         todays=parseInt(todays);
         res.render('result',{remaingdays:todays,hour:hour});
     }
