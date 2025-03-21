06c0596055beeacc5adfcfa78afe293221dcd1c2
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-03-21 13:03:14 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,25 +1,27 @@
-exports.getHome=(req,res,next)=>{
- res.render('index');
+exports.getHome = (req, res, next) => {
+    res.render('index');
 }
 
-exports.postHome=(req,res,next)=>{
-    const dob=req.body.dob;
-    const dobs=new Date(dob);
-    const avgHumnlife=70;
-    const date=new Date();
-    let dif=date.getTime()-dobs.getTime();
-    dif=dif/(1000*60*60*24*365.25);
-    dif=Math.floor(dif);
-    let total_life_year=avgHumnlife-dif;
-    if(total_life_year>0){
-        let todays=total_life_year*365.25;
-        let hour=todays-parseInt(todays);
-        hour=hour*24;
-        todays=parseInt(todays);
-        res.render('result',{remaingdays:todays,hour:hour});
+exports.postHome = (req, res, next) => {
+    const dob = req.body.dob;
+    const dobs = new Date(dob);
+    const avgHumnlife = 70;
+    const date = new Date();
+    let dif = date - dobs;
+    console.log("yer", dif);
+    dif = dif / (1000 * 60 * 60 * 24 * 365.25);
+    let total_life_year = (avgHumnlife) - (dif);
+    if (total_life_year > 0) {
+        let complet_year = Math.floor(total_life_year);
+        let desimalPart = total_life_year - complet_year;
+        let extra_days = desimalPart * 365.25;
+        let extra_decimal = extra_days - Math.floor(extra_days);
+        let hour = Math.floor(extra_decimal * 24);
+        complet_year = Math.floor(complet_year * 365.25) + Math.floor(extra_days);
+        res.render('result', { remaingdays: complet_year, hour: hour });
     }
-    else{
-        res.render('result',{remaingdays:"0",hour:"0"});
+    else {
+        res.render('result', { remaingdays: "0", hour: "0" });
     }
-    
+
 }
